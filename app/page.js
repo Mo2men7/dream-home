6685d35e653d8062fc1f9a28ede1367dc471532c
Momen Helmy <momen.helmy.m@gmail.com>
2024-10-28 11:10:30 +0300

    Small section of homepage

--- app/page.js
+++ app/page.js
@@ -1,3 +1,19 @@
+import { format } from "date-fns";
+import flag from "@/public/flag.svg";
+import Image from "next/image";
+
 export default function Home() {
-  return <div></div>;
+  const todayDate = format(new Date(), "EEE, dd MMM yyyy");
+
+  return (
+    <div>
+      <section className="flex items-center justify-between gap-2 text-gray-500 my-2">
+        <span className="">{todayDate}</span>
+        <div className="flex items-center gap-1">
+          <Image src={flag} alt="saudi arabia flag" width={20} height={20} />
+          <span>Saudi Arabia</span>
+        </div>
+      </section>
+    </div>
+  );
 }
